59f49b2aa440db3d2507652a5edb87a964822cdc
Savannah Clarke <92831573+savieclarke@users.noreply.github.com>
2021-11-27 18:00:31 -0500

    added instructions

---
+++ script.js
@@ -0,0 +1,6 @@
+//click start button
+//timer starts and presented with question
+//answer question correctly - presented with another question
+//answer incorrectly - time subtracted from the clock
+//game ends when all questions answered correctly or timer goes to 0
+//ability to save initials and score
