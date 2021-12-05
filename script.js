80be9c498a711d39d1b05b0422e9e7845e034ec6
Savannah Clarke <92831573+savieclarke@users.noreply.github.com>
2021-12-05 18:37:55 -0500

    made the question appear when clicking start

--- script.js
+++ script.js
@@ -4,3 +4,53 @@
 //answer incorrectly - time subtracted from the clock
 //game ends when all questions answered correctly or timer goes to 0
 //ability to save initials and score
+
+const startButton = document.getElementById('startb')
+startButton.addEventListener('click', startGame)
+
+var questionElement = document.getElementById('question')
+var answerButtonsElement = document.getElementById('answer-buttons')
+
+
+var questionContainerElement = document.getElementsByClassName ('question-container')
+
+var shuffledQuestions, currentQuestionIndex
+
+
+function startGame() {
+console.log("game start")
+startButton.classList.add('hide')
+shuffledQuestions = questions.sort(() => Math.random() - .5)
+currentQuestionIndex = 0
+//questionContainerElement.classList.remove('hide')
+nextQuestion()
+}
+
+function nextQuestion() {
+showQuestion(shuffledQuestions[currentQuestionIndex])
+
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+
+}
+
+function selectAnswer() {
+
+
+}
+
+const questions = [
+    {
+        question: 'Arrays in javascript can be used to store',
+        answers: [
+            {text: 'Numbers and strings', correct: true},
+            {text: 'Other arrays', correct: false},
+            {text: 'Booleans', correct: false},
+            {text: 'All of the above',  correct: false},
+
+
+        ]
+    }
+]
