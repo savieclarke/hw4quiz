c233d65b237ee56a7f75ab3874a6c500f97c3150
Savannah Clarke <92831573+savieclarke@users.noreply.github.com>
2021-12-25 00:48:47 -0500

    question and answer html

---
+++ questions.js
@@ -0,0 +1,55 @@
+/*{
+    question: 'Arrays in javascript can be used to store',
+    answers: [
+        {text: 'Numbers and strings', correct: false},
+        {text: 'Other arrays', correct: false},
+        {text: 'Booleans', correct: false},
+        {text: 'All of the above',  correct: true},
+
+    ]
+},
+{
+    question: 'Inside which HTML element do we put the JavaScript?',
+    answers: [
+        {text: '<js>', correct: false},
+        {text: '<script>', correct: true},
+        {text: '<javascript>', correct: false},
+        {text: '<scripting>',  correct: false},
+
+    ]
+},
+
+{
+    question: 'How do you create a function in Javascript?',
+    answers: [
+        {text: 'function myFunction()', correct: true},
+        {text: 'function = myFunction()', correct: false},
+        {text: 'function:myFunction()', correct: false},
+        {text: 'function myFunction',  correct: false},
+
+    ]
+},
+
+{
+    question: 'How to write an IF statement in JavaScript?',
+    answers: [
+        {text: 'if i = 5 then', correct: false},
+        {text: 'if i == 5 then', correct: false},
+        {text: 'if i = 5', correct: false},
+        {text: 'if (i == 5)'  , correct: true},
+
+    ]
+    
+},
+
+
+{
+    question: 'How do you write "Hello World" in an alert box?',
+    answers: [
+        {text: 'msgBox("Hello World");', correct: false},
+        {text: 'alertBox("Hello World");', correct: false},
+        {text: 'msg("Hello World");', correct: false},
+        {text: 'alert("Hello World"); ',  correct: true},
+
+    ]
+}))}]
